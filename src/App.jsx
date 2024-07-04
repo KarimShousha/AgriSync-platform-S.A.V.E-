67b7831c609535801520d746d8b05bdd7f1a948f
KarimShousha <106620842+KarimShousha@users.noreply.github.com>
2024-07-04 20:04:41 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Home from './pages/Home.jsx';
-import Monitoring from './pages/Monitoring.jsx';
-import Controlling from './pages/Controlling.jsx';
 import NavBar from './Components/NavBar.jsx';
-import Login from './pages/Login.jsx';
-import Register from './pages/Register.jsx';
-import DiseaseDetection from './pages/DiseaseDetection.jsx';
+import LoadingScreen from '../src/Components/loading.jsx';
 import { AuthProvider } from './AuthContext';
+const Home = lazy(() => import('./pages/Home.jsx'));
+const Monitoring = lazy(() => import('./pages/Monitoring.jsx'));
+const Controlling = lazy(() => import('./pages/Controlling.jsx'));
+const Login = lazy(() => import('./pages/Login.jsx'));
+const Register = lazy(() => import('./pages/Register.jsx'));
+const DiseaseDetection = lazy(() => import('./pages/DiseaseDetection.jsx'));
 
 function App() {
   return (
@@ -15,14 +16,16 @@ function App() {
       <AuthProvider>
         <div>
           <NavBar />
-          <Routes>
-            <Route path="/" element={<Navigate replace to="/login" />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/monitoring" element={<Monitoring />} />
-            <Route path="/controlling" element={<Controlling />} />
-          </Routes>
+          <Suspense fallback={<LoadingScreen />}>
+            <Routes>
+              <Route path="/" element={<Navigate replace to="/login" />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/monitoring" element={<Monitoring />} />
+              <Route path="/controlling" element={<Controlling />} />
+            </Routes>
+          </Suspense>
         </div>
       </AuthProvider>
     </Router>
@@ -30,53 +33,3 @@ function App() {
 }
 
 export default App;
-
-// src/App.js
-// import React, { useState, useEffect } from 'react';
-// import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-// import Home from './pages/Home.jsx';
-// import Monitoring from './pages/Monitoring.jsx';
-// import Controlling from './pages/Controlling.jsx';
-// import NavBar from './Components/NavBar.jsx';
-// import Login from './pages/Login.jsx';
-// import Register from './pages/Register.jsx';
-// import DiseaseDetection from './pages/DiseaseDetection.jsx';
-// import { AuthProvider } from './AuthContext';
-// import Loading from '../src/Components/loading.jsx';
-
-// function App() {
-//   const [loading, setLoading] = useState(true);
-
-//   useEffect(() => {
-//     // Simulate a loading delay
-//     const timer = setTimeout(() => {
-//       setLoading(false);
-//     }, 1000); // Adjust the time as needed
-
-//     return () => clearTimeout(timer);
-//   }, []);
-
-//   if (loading) {
-//     return <Loading />;
-//   }
-
-//   return (
-//     <Router>
-//       <AuthProvider>
-//         <div>
-//           <NavBar />
-//           <Routes>
-//             <Route path="/" element={<Navigate replace to="/login" />} />
-//             <Route path="/login" element={<Login />} />
-//             <Route path="/register" element={<Register />} />
-//             <Route path="/home" element={<Home />} />
-//             <Route path="/monitoring" element={<Monitoring />} />
-//             <Route path="/controlling" element={<Controlling />} />
-//           </Routes>
-//         </div>
-//       </AuthProvider>
-//     </Router>
-//   );
-// }
-
-// export default App;
