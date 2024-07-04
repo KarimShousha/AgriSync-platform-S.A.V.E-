08177254560b10e328813ea50ad2aaaa892e7d9c
KarimShousha <106620842+KarimShousha@users.noreply.github.com>
2024-07-04 20:48:12 +0300

    Update HomeContent.jsx

--- src/Components/HomeContent.jsx
+++ src/Components/HomeContent.jsx
@@ -7,17 +7,12 @@ import pivot2 from '../images/pivot2.jpg'
 export default function HomeContent(){
     return(
         <Fragment className="home-content">
-         {/* <h1>Electronics and Communications department Project Web Application (Smart Clean Farm) </h1>
-        <p>At Smart Clean Farm, we are revolutionizing the future of farming. Our mission is to combine cutting-edge technology with sustainable practices to create a cleaner, smarter, and more efficient farming experience.</p>
-        <p> We believe in the power of innovation to transform agriculture. Our advanced farming solutions leverage the latest in AI and IoT technologies to optimize crop growth, reduce waste, and increase overall yield.</p>
-        <p>But we’re not just about technology. We’re also deeply committed to sustainability. Our smart farming methods minimize the use of water and fertilizers, reducing our environmental footprint and making agriculture more sustainable for future generations.</p>
-        <p>Join us on our journey to make farming smarter, cleaner, and greener. Welcome to the future of agriculture. Welcome to Smart Clean Farm.</p>  
-        <h3>Supervised by Prof/ Heba Youssef Soliman</h3>  */}
     <div className="HPic-1">
     <div className="image1">
     <div className="home1card">
         <h2 className='pivotHeader'>AgriSync Platform</h2>
         <p>AgriSync streamlines irrigation management, providing real-time insights and precision control for optimal crop yield.</p>
+        <h3>Supervised by Prof. Dr. Heba Youssef Soliman</h3>
     </div>
     </div>
     </div>
@@ -43,9 +38,8 @@ export default function HomeContent(){
         <img src={pivot1} alt="pivot1" className='pivot1' />
         <div className="textBlock">
             <h2>03</h2>
-            <h3>Boosting Yields, One Circle at a Time</h3>
-            <p>By providing consistent watering,
-            pivot irrigation allows you to maximize crop yields. This translates to a more profitable harvest and a greater return on your investment.</p>
+            <h3>Crop Health Monitoring</h3>
+            <p>AgriSync integrates cameras and sensors to detect anomalies, monitor crop health, and prevent issues before they escalate. From weed detection to disease prevention, our platform keeps crops thriving.</p>
         </div>
     </div>
 
@@ -53,9 +47,8 @@ export default function HomeContent(){
         <img src={pivot2} alt="pivot2" className='pivot2' />
         <div className="textBlock">
             <h2>04</h2>
-            <h3>Boosting Yields, One Circle at a Time</h3>
-            <p>By providing consistent watering,
-            pivot irrigation allows you to maximize crop yields. This translates to a more profitable harvest and a greater return on your investment.</p>
+            <h3>Sustainability Boost</h3>
+            <p>By reducing water consumption and minimizing runoff, pivot irrigation contributes to sustainable farming practices. AgriSync empowers farmers to be stewards of the land while maximizing productivity.</p>
         </div>
     </div>
 </div>
